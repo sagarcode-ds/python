ebf67787bb91d54f237f15ceba0e945440274912
Sagarcode-ds <sagar.bhattarai2062@gmail.com>
2026-06-02 16:06:09 +0545

    i finished file reading and started file writing from python mooc 2026 by helsinky

--- helsinky.py/part6.py
+++ helsinky.py/part6.py
@@ -535,3 +535,154 @@
 #                 station2=st2
 #     return station1,station2,max_distance
 # print(greatest_distance(stations))
+
+# writing files
+
+# Please write a program which works as a simply diary. The diary entries should be saved in the file diary.txt. When the program is executed, it should first read any entries already in the file.
+# The program should work as follows:
+# Sample output
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 1
+# Diary entry: Today I ate porridge
+# Diary saved
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 2
+# Entries:
+# Today I ate porridge
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 1
+# Diary entry: I went to the sauna in the evening
+# Diary saved
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 2
+# Entries:
+# Today I ate porridge
+# I went to the sauna in the evening
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 0
+# Bye now!
+
+# When the program is executed for the second time, this should happen:
+
+# Sample output
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 2
+# Entries:
+# Today I ate porridge
+# I went to the sauna in the evening
+# 1 - add an entry, 2 - read entries, 0 - quit
+# Function: 0
+# Bye now!
+# def read_file(filename):
+#     try:
+#         with open(filename) as f:
+#             content=f.read()
+#         if content=='':
+#             print('no entries available')
+#         else:
+#             print(content.strip())
+#     except FileNotFoundError:
+#             print('File does not exist')
+
+            
+            
+
+# def write_file(filename):
+#     text=input('Diary entry:')
+#     with open(filename,'a') as f:
+#         f.write(text+'\n')
+#         print('Diary saved')
+    
+# read_file('diary.txt')
+# while True:
+#     print('1 - add an entry, 2 - read entries, 0 - quit')
+#     c=input('function:')
+#     if c=='0':
+#         print('Bye now!')
+#         break
+#     elif c=='1':
+#         write_file('diary.txt')
+#     elif c=='2':
+#         print('Entries:')
+#         read_file('diary.txt')
+    
+# The file solutions.csv contains some solutions to mathematics problems:
+# Arto;2+5;7
+# Pekka;3-2;1
+# Erkki;9+3;11
+# Arto;8-3;4
+# Pekka;5+5;10
+# ...jne...
+# As you can see above, on each line the format is name_of_student;problem;result. All the operations are either addition or subtraction, and each has exactly two operands.
+# Please write a function named filter_solutions() which
+# Reads the contents of the file solutions.csv
+# writes those lines which have a correct result into the file correct.csv
+# writes those lines which have an incorrect result into the file incorrect.csv
+# Using the example above, the file correct.csv would contain the lines
+
+# Arto;2+5;7
+# Pekka;3-2;1
+# Pekka;5+5;10
+# The other two would be in the file incorrect.csv.
+# Please write the lines in the same order as they appear in the original file. Do not change the original file.
+# NB: the function should have the exact same result, no matter how many times it is called. That is, it shouldn't matter if the function is called once
+# filter_solutions()
+# or multiple times in a row
+# filter_solutions()
+# filter_solutions()
+# filter_solutions()
+# filter_solutions()
+# After the execution, the contents of the files correct.csv and incorrect.csv should be exactly the same in either case.   
+
+# def filter_solutions():
+#     correct=[]
+#     incorrect=[]
+#     with open('solutions.csv') as f:
+#         for line in f:
+#             parts=line.strip().split(';')
+#             if '+' in parts[1]:
+#                 a,b=parts[1].split('+')
+#                 if int(a)+int(b)==int(parts[2]):
+#                     correct.append(line.strip())
+#                 else:
+#                     incorrect.append(line.strip())
+#             elif '-' in parts[1]:
+#                 x,y=parts[1].split('-')
+#                 if int(x)-int(y)==int(parts[2]):
+#                         correct.append(line.strip())
+#                 else:
+#                     incorrect.append(line.strip())
+#             else:
+#                 incorrect.append(line.strip())
+                          
+#     with open('correct.csv','w') as f:
+#         for line in correct:
+#             f.write(line+'\n')
+#     with open('incorrect.csv','w') as f:
+#         for line in incorrect:
+#             f.write(line+'\n') 
+
+# filter_solutions()
+# filter_solutions()             
+# filter_solutions()
+
+# Please write a function named store_personal_data(person: tuple), which takes a tuple containing some identifying information as its argument.
+# The tuple contains the following elements:
+# Name (string)
+# Age (integer)
+# Height (float)
+# This should be processed and written into the file people.csv. The file may already contain some data; the new entry goes to the end of the file. The data should be written in the format
+# name;age;height
+# Each entry should be on a separate line. If we call the function with the argument ("Paul Paulson", 37, 175.5), the function should write this line to the end of the file:
+# Paul Paulson;37;175.5
+
+# def store_personal_data(person: tuple):
+#     line=''
+#     for i in person:
+#         line+=f'{i};'
+#     line=line[:-1]
+#     with open('people.csv','a') as f:
+#         f.write(line+'\n')
+# store_personal_data(('sagar',12,144))
+# store_personal_data(('bob',12,5.6))
+
